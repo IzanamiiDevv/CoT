94bd74f01684a753cd00575907e97af32145d277
IzanamiiDevv <rafaeloli596@gmail.com>
2024-10-09 23:34:31 +0800

    Modify some code

--- variables and data types/Basics/variables.cpp
+++ variables and data types/Basics/variables.cpp
@@ -1,3 +1,4 @@
+// !: variables.cpp
 #include <iostream>
 #include <string>
 
@@ -5,20 +6,29 @@ int main() {
     std::string name; //               *: Declare a string variable.
     name = "User Name"; //             *: Initialize a variable.
 
-    std::cout << name << std::endl; // *: Print out the value of the variable.
+    std::cout << name << std::endl; // *: Prints out a value of the variable.
 
-    /**
-     * !: Note: Each primative value is stored in Stack Memorry.
-     *  *: Lexical Scoping
-     */
+    // !: Note: Each primative type is stored in Stack Memorry.
+    // *: Lexical Scoping.
     {
         int x = 10;
-    } // *: x variable will free to memorry in this line.
+    } // *: x variable will free to memorry in this line due to stack behevior.
 
     //std::cout << x << std::endl;  // *: Throws and Error due to x variable lifetime.
+
+    /**
+     * *: Copying a value of inner variable to the num variable will saves the value for deleting
+     * *: But the context of the value will be deleted.
+     */
+    int num;
+    {
+        int inner_num = 10; //         *: Delclares a inner variable that will be deleted on line: 27 
+        num = inner_num; //            *: Initialize the num with the value of inner_num.
+    } //                               *: variable: inner_num will be deleted in this line.
+
     
 
-
+    std::cout << num << std::endl; //  *: Prints out the value from num that comes from inner_num.
 
     return 0; //                       *: Returns 0 for sucessfull execution
 }
